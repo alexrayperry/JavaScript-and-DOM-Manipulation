ca0cc9b7b88a72cc49eca7f6bd23dd2657e6f6cb
Alex Perry <alexrayperry@gmail.com>
2020-12-15 21:59:47 -0800

    wrote JS code to retrieve data from data.js and write data into a table on HTML file utilizing tbody tag.

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,26 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select the "tbody" tag using d3. "tbody" will be used to manipulate the table area of the webage.
+
+var tbody = d3.select("tbody")
+
+// Loop through each object in the dataset
+
+tableData.forEach((entry) => {
+
+    // Define new row in table using "tr" tag
+
+    var row = tbody.append("tr");
+
+    // Iterate through JS objects and simultaneously retrieve values from key/value pairs
+    // and add values to HTML table using previously defined "row" variable and appending "td" tag.
+
+    Object.entries(entry).forEach(([key,value]) => {
+
+        var ufoInfo = row.append("td");
+
+        ufoInfo.text(value);
+
+    });
+});
